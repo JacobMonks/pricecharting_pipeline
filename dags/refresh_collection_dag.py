eb27af1ab48c96c56a4a30a161716e64d59dcd2b
Jacob Monks <jacobmonks0408@gmail.com>
2025-07-12 10:33:45 -0400

    Creates Docker yaml and environment files.

---
+++ dags/refresh_collection_dag.py
@@ -0,0 +1,22 @@
+import datetime
+from airflow.decorators import dag, task
+
+
+@dag(
+        dag_id="refresh_collection",
+        start_date=datetime.datetime(2025, 7, 12),
+        schedule="0 10 * * 0",
+        tags=["sunday", "weekly"],
+        catchup=False,
+        default_args={"retries": 2, "owner": "Jacob"}
+)
+def refresh_collection():
+
+    @task()
+    def dummy_task(**kwargs):
+        pass
+
+    dummy_task()
+
+
+refresh_collection()
