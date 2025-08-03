07eca15f1ccb3c320fe928c2e3df526dbd7cfcd1
Jacob Monks <jacobmonks0408@gmail.com>
2025-08-02 23:37:02 -0400

    Created constants file.

--- dags/src/gsheet_migration.py
+++ dags/src/gsheet_migration.py
@@ -1,6 +1,6 @@
 import os.path
 import pandas as pd
-from private_constants import Constants
+from dags.src.constants import Constants
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
