5e1cacce73cb55ce0ddb5a7de7dd42a7f663fffc
Jacob Monks <jacobmonks0408@gmail.com>
2025-08-09 15:15:02 -0400

    Query function returns any number of columns.

--- dags/src/mysql_utils.py
+++ dags/src/mysql_utils.py
@@ -2,6 +2,7 @@ from mysql import connector
 
 
 def query_no_filter(database_name: str, table_name: str, columns):
+    results = []
     try:
         conn = connector.connect(
             host="127.0.0.1",
@@ -13,21 +14,21 @@ def query_no_filter(database_name: str, table_name: str, columns):
         cursor = conn.cursor()
 
         cols = None
-        num_cols = 0
+        cols_list = None
         if isinstance(columns, list):
+            cols_list = columns
             cols = ", ".join(columns)
-            num_cols = len(columns)
 
         elif isinstance(columns, str):
+            cols_list = columns.split(',')
             cols = columns
-            num_cols = len(cols.split(','))
 
         query_row_sql = f"SELECT {cols} FROM {table_name} limit 10;"
         cursor.execute(query_row_sql)
 
-        print(cols, num_cols)
+        num_cols = len(cols_list)
         for row in cursor:
-            print(row[0])
+            results.append({cols_list[num]: row[num] for num in range(num_cols)})
 
         cursor.close()
         conn.close()
@@ -35,6 +36,9 @@ def query_no_filter(database_name: str, table_name: str, columns):
     except connector.Error as e:
         print("ERROR - ", e)
 
+    return results
+
 
 if __name__ == "__main__":
-    query_no_filter("pricecharting", "vg_price", "title_tag")
+    results = query_no_filter("pricecharting", "vg_price", "title_tag")
+    print(results)
